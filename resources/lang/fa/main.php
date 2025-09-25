588ac77207f5384645e3657106241b144ca133fa
Hosein Safdel <hetbo98@yahoo.com>
2025-09-25 17:04:17 +0000

    Added Language Files

---
+++ resources/lang/fa/main.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    'admin' => 'مدیر',
+    'teacher' => 'مدرس',
+    'student' => 'دانشجو',
+
+    'verify-mobile' => 'تلفن همراه خود را تایید کنید',
+
+    'register' => 'ثبت نام',
+
+    'auth-heading' => '"تجربه یادگیری خود را متحول کنید',
+    'auth-subheading' => 'مهارت‌های جدید را با سرعت خودتان فرا بگیرید',
+    'auth-bottom' => 'جایی که دانش با نوآوری ملاقات می‌کند',
+
+    'create-account' => 'ایجاد حساب کاربری',
+    'join-us-text' => 'برای شروع به ما بپیوندید',
+    'full-name' => 'نام کامل',
+    'enter-full-name' => 'نام کامل خود را وارد کنید',
+    'email-address' => 'آدرس ایمیل',
+    'optional' => 'اختیاری',
+    'email-placeholder' => 'your.email@example.com',
+    'mobile-number' => 'شماره موبایل',
+    'mobile-placeholder' => '09123456789',
+    'role' => 'نقش',
+    'select-role' => 'نقش خود را انتخاب کنید',
+    'password' => 'رمز عبور',
+    'create-password' => 'یک رمز عبور قوی ایجاد کنید',
+    'confirm-password' => 'تأیید رمز عبور',
+    'reenter-password' => 'رمز عبور خود را دوباره وارد کنید',
+    'agree-to' => 'موافقت با',
+    'terms-service' => 'شرایط خدمات',
+    'and' => 'و',
+    'privacy-policy' => 'سیاست حفظ حریم خصوصی',
+    'continue-with' => 'یا ادامه دهید با',
+    'google-continue' => 'ادامه با گوگل',
+    'github-continue' => 'ادامه با گیت‌هاب',
+    'already-account' => 'قبلاً حساب کاربری دارید؟',
+    'sign-in-here' => 'اینجا وارد شوید',
+
+    'validation-password-confirm' => 'رمز عبور با تکرار آن مطابقت ندارد',
+    'validation-mobile' => 'یک شماره صحیح وارد نمایید',
+
+];
